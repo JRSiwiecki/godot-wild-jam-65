a7fd5ef8320d4db6336e9a7b87248a343c76ee08
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-15 17:09:01 -0800

    Add laser attack + timer

--- scripts/Tower.gd
+++ scripts/Tower.gd
@@ -8,6 +8,10 @@ signal powered
 @export var aoe_attack_area : Area2D
 @export var aoe_attack_timer : Timer
 
+@export var laser_attack_area : Area2D
+@export var laser_attack_timer : Timer
+@export var laser_attack_raycast : RayCast2D
+
 @export var MAX_HEALTH : int = 100
 
 var current_health : int = MAX_HEALTH
@@ -19,7 +23,7 @@ var can_laser_attack : bool = true
 var can_missile_attack : bool = true
 var can_spiral_attack : bool = true
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if can_aoe_attack and power >= 0:
 		aoe_attack()
 	
@@ -38,18 +42,53 @@ func aoe_attack() -> void:
 		if body is Enemy:
 			body.death()
 	
-	# Reset AOE Attack
+	# Reset AOE attack
 	aoe_attack_timer.start()
 	can_aoe_attack = false
 
 func laser_attack() -> void:
-	pass
+	# Find all enemies in laser range
+	var enemies : Array = []
+	
+	for body in laser_attack_area.get_overlapping_bodies():
+		if body is Enemy:
+			enemies.append(body)
+	
+	# Find closest enemy
+	var closest_enemy : Node2D = find_closest_enemy(enemies)
+	
+	# No enemy found
+	if !closest_enemy:
+		return
+	
+	# Attack enemy with laser
+	laser_attack_raycast.target_position = closest_enemy.global_position
+	var enemy_being_attacked : Object = laser_attack_raycast.get_collider()
+	
+	if enemy_being_attacked and enemy_being_attacked.has_method("death"):
+		enemy_being_attacked.death()
+	
+	# Reset laser attack
+	laser_attack_timer.start()
+	can_laser_attack = false
 
 func missile_attack() -> void:
 	pass
 
 func spiral_attack() -> void:
 	pass
+
+func find_closest_enemy(enemies : Array) -> Node2D:
+	var closest_enemy = null
+	var shortest_distance : float = 100000.0
+	for enemy in enemies:
+		var distance : float = global_position.distance_to(enemy.global_position)
+		
+		if distance < shortest_distance:
+			shortest_distance = distance
+			closest_enemy = enemy
+	
+	return closest_enemy
 
 func _on_damage_area_body_entered(body: Node2D) -> void:
 	if body is Enemy:
@@ -71,3 +110,7 @@ func _on_power_deposit_area_body_entered(body: Node2D) -> void:
 
 func _on_aoe_attack_timer_timeout() -> void:
 	can_aoe_attack = true
+
+
+func _on_laser_attack_timer_timeout() -> void:
+	can_laser_attack = true
