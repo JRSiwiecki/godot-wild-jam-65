7cdd20056fc467e0e8e2c1164c2f84102092fb2e
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-16 16:11:33 -0800

    Add new battery types + textures

--- scripts/Globals.gd
+++ scripts/Globals.gd
@@ -1,3 +1,4 @@
 extends Node
 
-var power_per_battery : int = 25
+var power_per_normal_battery : int = 25
+var power_per_special_battery : int = 15
--- scripts/Power.gd
+++ scripts/Power.gd
@@ -4,7 +4,32 @@ class_name Power
 
 signal collected
 
+@export var sprite : Sprite2D
+
+@export var power_texture : Texture2D
+@export var health_texture : Texture2D
+@export var shield_texture : Texture2D
+@export var speed_texture : Texture2D
+
 @onready var player : Player = get_node("/root/Game/Player")
+
+enum POWER_TYPE { POWER, HEALTH, SHIELD, SPEED } 
+
+var power_type : POWER_TYPE
+
+func _ready() -> void:
+	change_sprite_color()
+
+func change_sprite_color() -> void:
+	match power_type:
+		POWER_TYPE.POWER:
+			sprite.texture = power_texture
+		POWER_TYPE.HEALTH:
+			sprite.texture = health_texture
+		POWER_TYPE.SHIELD:
+			sprite.texture = shield_texture
+		POWER_TYPE.SPEED:
+			sprite.texture = speed_texture
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player and player.power_carried < player.CARRY_CAPACITY:
