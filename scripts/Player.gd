918d7b858fe0099b4d51a7a74363bd8018f119f7
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-20 23:12:06 -0800

    Add running animations

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 class_name Player
 
+@export var animated_sprite : AnimatedSprite2D
+
 @export var speed : float = 1500.0
 @export var CARRY_CAPACITY : int = 50
 
@@ -11,6 +13,13 @@ var power_types_carried : Array = []
 func _physics_process(_delta: float) -> void:
 	var direction : Vector2 = Input.get_vector("left", "right", "up", "down").normalized()
 	velocity = direction * speed
+	
+	if velocity == Vector2.ZERO:
+		animated_sprite.play("idle")
+	
+	else:
+		animated_sprite.play("run")
+		animated_sprite.flip_h = velocity.x <= 0
 	
 	move_and_slide()
 
