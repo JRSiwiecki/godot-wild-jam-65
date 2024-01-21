91ad3c6f453fe5a7853ed8911b50935e7d5713d2
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-21 02:16:16 -0800

    Add background colors to various menus

--- scripts/Game Over.gd
+++ scripts/Game Over.gd
@@ -1,15 +1,17 @@
 extends Control
 
+@export var background : ColorRect
 @export var outcome_label : Label
-
 @export var victory_music : AudioStreamPlayer
 @export var defeat_music : AudioStreamPlayer
 
 func _ready() -> void:
 	if Globals.game_outcome:
+		background.color = Color.DARK_GREEN
 		outcome_label.text = "You win!"
 		victory_music.play()
 	else:
+		background.color = Color.INDIAN_RED
 		outcome_label.text = "You lost!"
 		defeat_music.play()
 
