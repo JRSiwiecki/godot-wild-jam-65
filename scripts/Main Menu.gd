811b48bec61b78f9d10684c4b1e4c0f6bcba2ba0
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-21 00:14:24 -0800

    Add main menu music, font

--- scripts/Main Menu.gd
+++ scripts/Main Menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 @export var game_scene : PackedScene
 
+@export var main_menu_music : AudioStreamPlayer
+
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
 
