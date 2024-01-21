860bd0e6c10fbeb3d8d4603a78f4b6cbd36176ee
JRSiwiecki <JRSiwiecki@gmail.com>
2024-01-21 00:17:48 -0800

    Add victory/defeat music

--- scripts/Game Over.gd
+++ scripts/Game Over.gd
@@ -1,14 +1,17 @@
 extends Control
 
-@export var canvas_layer : CanvasLayer
-
 @export var outcome_label : Label
+
+@export var victory_music : AudioStreamPlayer
+@export var defeat_music : AudioStreamPlayer
 
 func _ready() -> void:
 	if Globals.game_outcome:
 		outcome_label.text = "You win!"
+		victory_music.play()
 	else:
 		outcome_label.text = "You lost!"
+		defeat_music.play()
 
 
 func _on_quit_button_pressed() -> void:
